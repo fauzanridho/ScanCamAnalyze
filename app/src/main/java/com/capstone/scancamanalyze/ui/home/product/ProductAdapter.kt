8ac5dde816881ae9711a06361ba97ad9794a98af
zaan da vinci <Zanci@gmail.com>
2024-11-24 10:18:23 +0700

    membuat halaman untuk detail masing" produk

--- app/src/main/java/com/capstone/scancamanalyze/ui/home/product/ProductAdapter.kt
+++ app/src/main/java/com/capstone/scancamanalyze/ui/home/product/ProductAdapter.kt
@@ -1,22 +1,27 @@
 package com.capstone.scancamanalyze.ui.home.product
 
-import android.util.Log
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.capstone.scancamanalyze.databinding.ItemProductBinding
+import com.capstone.scancamanalyze.ui.home.product.detailproduct.DetailProduct
 
 class ProductAdapter(private val products: List<Product>) :
     RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
     class ProductViewHolder(private val binding: ItemProductBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(product: Product) {
+        fun bind(product: Product, onClick: (Product) -> Unit) {
             binding.tvProductName.text = product.name
             binding.tvProductBrand.text = product.brand
             binding.tvProductDescription.text = product.description
             binding.tvProductPrice.text = product.price
             binding.ivProductImage.setImageResource(product.imageResId)
+
+            binding.root.setOnClickListener {
+                onClick(product)
+            }
         }
     }
 
@@ -26,8 +31,16 @@ class ProductAdapter(private val products: List<Product>) :
     }
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-        Log.d("ProductAdapter", "Binding data untuk produk: ${products[position].name}")
-        holder.bind(products[position])
+        val product = products[position]
+        holder.bind(product) {
+            val intent = Intent(holder.itemView.context, DetailProduct::class.java)
+            intent.putExtra("EXTRA_PRODUCT_NAME", product.name)
+            intent.putExtra("EXTRA_PRODUCT_BRAND", product.brand)
+            intent.putExtra("EXTRA_PRODUCT_DESCRIPTION", product.description)
+            intent.putExtra("EXTRA_PRODUCT_PRICE", product.price)
+            intent.putExtra("EXTRA_PRODUCT_IMAGE", product.imageResId)
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     override fun getItemCount() = products.size
